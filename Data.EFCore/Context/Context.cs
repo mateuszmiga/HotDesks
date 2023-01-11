f9563c6ad6527c19c4f528e7c96ca38f48821ac1
DESKTOP-AVIEUQ2\Mateusz <mateusz.miga11@gmail.com>
2023-01-11 18:02:13 +0100

    added Context class and ConnectionString

---
+++ Data.EFCore/Context/Context.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.EFCore.Context
+{
+    public class Context : DbContext
+    {
+        
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+        
+        public DbSet<Desk> Desks { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+
+       
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+    }
+}
