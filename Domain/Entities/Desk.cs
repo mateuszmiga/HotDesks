c4b614e2172baebdbeeb366ec01e7bde2f2f9513
Mateusz Miga <mateusz.miga11@gmail.com>
2023-01-11 13:43:50 +0100

    Added Entities

---
+++ Domain/Entities/Desk.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Desk
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime RentingStart { get; set; }
+        public DateTime RentingEnd { get; set; }
+        public virtual Owner Owner { get; set; }
+        public virtual Room Room { get; set; }
+
+
+    }
+}
