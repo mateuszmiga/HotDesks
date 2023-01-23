b477a0c9e46f943ffebdfb3bb55e8d2abdf2f0ef
Mateusz Miga <mateusz.miga11@gmail.com>
2023-01-23 13:47:02 +0100

    implemented IGenericRepository

--- Data.EFCore/Repository/GenericRepository.cs
+++ Data.EFCore/Repository/GenericRepository.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace Data.EFCore.Repository
 {
-    internal class GenericRepository
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
     {
+        public void Create(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T obj)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
