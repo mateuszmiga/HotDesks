f6379eaadf5243838adce8a320154298aa8e12fd
Mateusz Miga <mateusz.miga11@gmail.com>
2023-04-20 15:03:06 +0200

    .

--- HotDesks/Services/AuthManager.cs
+++ HotDesks/Services/AuthManager.cs
@@ -1,7 +1,10 @@
 ﻿using Domain.Entities;
 using HotDesks.Api.Dto;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace HotDesks.Api.Services
 {
@@ -9,6 +12,7 @@ namespace HotDesks.Api.Services
     {
         private readonly UserManager<User> _userManager;
         private readonly IConfiguration _configuration;
+        private User _user;
 
         public AuthManager(IConfiguration configuration, UserManager<User> userManager)
         {
@@ -25,16 +29,42 @@ namespace HotDesks.Api.Services
             return new JwtSecurityTokenHandler().WriteToken(tokenOptions);
         }
 
-        private object GetSigningCredentials()
+        private JwtSecurityToken GenerateTokenOptions(SigningCredentials signingCredentials, List<Claim> claims)
         {
             throw new NotImplementedException();
         }
 
         public async Task<bool> ValidateUser(LoginUserDto loginUserDto)
         {
-            var user = await _userManager.FindByEmailAsync(loginUserDto.Email);
-            var result = (user != null && await _userManager.CheckPasswordAsync(user, loginUserDto.Password));
+            _user = await _userManager.FindByEmailAsync(loginUserDto.Email);
+            var result = (_user != null && await _userManager.CheckPasswordAsync(_user, loginUserDto.Password));
             return result;
         }
+
+        private async Task<List<Claim>> GetClaims()
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Email, _user.Email)
+            };
+
+            var roles = await _userManager.GetRolesAsync(_user);
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            return claims;
+        }
+
+        private SigningCredentials GetSigningCredentials()
+        {
+            var key = Environment.GetEnvironmentVariable("KEY");
+            var secret = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+
+            return new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
+        }
+
     }
 }
